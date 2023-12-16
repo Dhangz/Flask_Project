cba59b9aeb4d3e0fd91e265c7ee200c3de769ce2
HEBEN <202080491@psu.palawan.edu.ph>
2023-12-17 04:43:20 +0800

    add tests to the web app

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #Connect to dog_breeding database
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "dog_breeding"
 
 
---
+++ test.py
@@ -0,0 +1,28 @@
+import unittest
+from api import app
+import warnings
+
+class MyappTests(unittest.TestCase):
+  def setUp(self):
+    app.config["TESTING"] = True
+    self.app = app.test_client()
+
+
+    warnings.simplefilter("ignore", category=DeprecationWarning)
+    
+  def test_index_page(self):
+    response = self.app.get("/")
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+
+  def test_get_actor(self):
+    response = self.app.get("/dogs")
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue("Loxodonta africana" in response.data.decode())
+
+
+
+
+
+if __name__ == "__main__":
+  unittest.main()
