b18cb76bb74ac35b2247bbc62b80e8840c013d74
HEBEN <202080491@psu.palawan.edu.ph>
2023-12-23 09:32:52 +0800

    fix the CRUD

--- api.py
+++ api.py
@@ -1,93 +1,97 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, render_template, request, url_for, flash, jsonify
+from werkzeug.utils import redirect
 from flask_mysqldb import MySQL
 
 
-#name of my application
 app = Flask(__name__)
+app.secret_key = 'many random bytes'
 
-
-#Connect to dog_breeding database
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "dog_breeding"
-
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'dog_breeding'
 
 mysql = MySQL(app)
 
-
-
-#tell flask what url should trigger our function and return the message we want to display in the browser
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-def data_fetch(query):
-      cur = mysql.connection.cursor()
-      cur.execute(query)
-      data = cur.fetchall()
-      cur.close()
-      return data
-
-
-@app.route("/dogs", methods=["GET"])
-def get_dogs():
-   data = data_fetch(" SELECT * FROM dogs ")
-   return make_response(jsonify(data), 200)
-   
-
-@app.route("/dogs/<int:id>", methods=["GET"])
-def get_dog_by_id(id):
-   data = data_fetch(" SELECT * FROM dogs WHERE dog_id = {}".format(id))
-   return make_response(jsonify(data), 200)
-
-@app.route("/dogs/<int:id>/relationship_id", methods=["GET"])
-def get_dog_relationships(id):
-    data = data_fetch(""" 
-          SELECT relationships.relationship_id, dog_1_id, dog_2_id, relationship_Types.relationship_description
-          FROM relationships
-          JOIN dogs ON relationships.dog_1_id = dogs.dog_id
-          JOIN dogs AS dogs_2 ON relationships.dog_2_id = dogs_2.dog_id
-          JOIN relationship_Types ON relationships.relationship_code = relationship_Types.relationship_code;
-    """.format(id))
-    
-    return make_response(jsonify({"relationship_id":id, "relationship":data, "count":len(data), }), 200)
-
-
-# Create Dogs
-@app.route("/dogs", methods=["POST"])
-def add_actor():
+@app.route('/')
+def Index():
     cur = mysql.connection.cursor()
-    info = request.get_json()
-
-    dogs_name = info["dogs_name"]
-    born_in_litter_id = info["born_in_litter_id"]
-    gender_mf = info["gender_mf"]
-    date_of_birth = info["date_of_birth"]
-    place_of_birth = info["place_of_birth"]
-    other_details = info["other_details"]
-
-    cur.execute(
-            """INSERT INTO dogs (dogs_name, born_in_litter_id, gender_mf, date_of_birth, place_of_birth, other_details)
-               VALUES (%s, %s, %s, %s, %s, %s,) """, (dogs_name, born_in_litter_id, gender_mf, date_of_birth, place_of_birth, other_details)
-    )
-
-    mysql.connection.commit()
-    print("row(s) affected :{}".format(cur.rowcount))
-    rows_affected = cur.rowcount
+    cur.execute("SELECT * FROM dogs")
+    data = cur.fetchall()
     cur.close()
-    return make_response(jsonify({"message": "dog added sucessfully", "rows_affected": cur.rowcount}), 201)
+
+    return render_template('index.html', dogs=data)
 
 
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == "POST":
+        flash("Data Inserted Successfully")
+        
+        born_in_litter_id = request.form.get("born_in_litter_id")
+        dog_name = request.form.get("dog_name")
+        gender_mf = request.form.get("gender_mf")
+        date_of_birth = request.form.get("date_of_birth")
+        place_of_birth = request.form.get("place_of_birth")
+        other_details = request.form.get("other_details")
+
+        cur = mysql.connection.cursor()
+        cur.execute("""INSERT INTO dogs (born_in_litter_id, dog_name, gender_mf, date_of_birth, place_of_birth, other_details)
+               VALUES (%s, %s, %s, %s, %s, %s) """, ( born_in_litter_id, dog_name, gender_mf, date_of_birth, place_of_birth, other_details))
+
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
 
 
+@app.route('/update/<string:id>', methods=['POST', 'GET'])
+def update(id):
+    if request.method == 'POST':
+        dog_id = request.form.get("dog_id", None)
+        dog_name = request.form.get("dog_name", None)
+        gender_mf = request.form.get("gender_mf", None)
+        date_of_birth = request.form.get("date_of_birth", None)
+        place_of_birth = request.form.get("place_of_birth", None)
+        other_details = request.form.get("other_details", None)
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE dogs SET 
+            dog_name = %s, gender_mf = %s, date_of_birth = %s, place_of_birth = %s, other_details = %s
+            WHERE dog_id = %s
+        """, (dog_name, gender_mf, date_of_birth, place_of_birth, other_details, dog_id))
+        mysql.connection.commit()  # Add this line to commit the changes
+        flash("Data Updated Successfully")
+        return redirect(url_for('Index'))
+
+    
+
+@app.route('/delete/<string:id>', methods = ['GET'])
+def delete(id):
+    print(f"Deleting record with id: {id}")
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM dogs WHERE dog_id=%s", (id,))
+    mysql.connection.commit()
+    return redirect(url_for('Index'))
 
 
+@app.route('/api', methods=['GET'])
+def api():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM dogs")
+    data = cur.fetchall()
+    cur.close()
 
+    format_type = request.args.get('format', 'json')  # Default to JSON if not specified
 
+    if format_type == 'xml':
+        response = app.response_class(response=render_template('data.xml', dogs=data), status=200, mimetype='application/xml')
+    elif format_type == 'json':
+        response = jsonify(dogs=data)
+    else:
+        response = jsonify({'error': 'Invalid format specified'})
 
-
+    return response
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,33 +1,47 @@
 import unittest
-from api import app
+from api import app  # Assuming your Flask app is in a file named api.py
 import warnings
 
-class MyappTests(unittest.TestCase):
-  def setUp(self):
-    app.config["TESTING"] = True
-    self.app = app.test_client()
+class MyAppTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+        warnings.simplefilter("ignore", category=DeprecationWarning)
 
+    def test_index(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Python Flask CRUD Application", response.data)
 
-    warnings.simplefilter("ignore", category=DeprecationWarning)
-    
-  def test_index_page(self):
-    response = self.app.get("/")
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+    def test_insert(self):
+        response = self.app.post("/insert", data=dict(
+            born_in_litter_id=1,
+            dog_name="browny",
+            gender_mf="M",
+            date_of_birth="2023-01-01",
+            place_of_birth="puerto princesa city",
+            other_details="friendly"
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Data Inserted Successfully", response.data)
 
-  def test_get_dogs(self):
-    response = self.app.get("/dogs")
-    self.assertEqual(response.status_code, 200)
-    self.assertTrue("Loxodonta africana" in response.data.decode())
+    def test_update(self):
+        response = self.app.post("/update/5", data=dict(
+            dog_id=5,
+            dog_name="blacky",
+            gender_mf="M",
+            date_of_birth="2023-02-02",
+            place_of_birth="Narra, Palawan",
+            other_details="Bad dog"
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Data Updated Successfully", response.data)
 
-  def test_get_dog_by_id(self):
-    response = self.app.get("/dogs/1")
-    self.assertEqual(response.status_code, 200)
-    self.assertTrue("Loxodonta africana" in response.data.decode())
-
-
-
+    def test_delete(self):
+        response = self.app.get("/delete/4", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Record Has Been Deleted Successfully", response.data)
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
